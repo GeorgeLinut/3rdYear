424f109b674d30247d37bc2eb390f4797c19467c
George Linut <George.Linut@catalysts.cc>
2017-11-28 16:41:06 +0200

    begin lab 2

--- Parallel and distributed programming/ro.ubb.lab6/generateTests.py
+++ Parallel and distributed programming/ro.ubb.lab6/generateTests.py
@@ -3,7 +3,7 @@ from random import randint
 T = [10, 50, 100, 500, 1000, 2000, 5000, 10000, 100000, 250000]
 
 def test(tid, n):
-  with open("tests/%d.in" % tid, "w") as f:
+  with open("tests%d.in" % tid, "w") as f:
     f.write("%d\n" % n)
     for i in range(n):
       f.write("%d " % randint(1, 100))
---
+++ Parallel and distributed programming/ro.ubb.lab6/main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+
+
+
+main::main()
+{
+}
+
+
+main::~main()
+{
+}
